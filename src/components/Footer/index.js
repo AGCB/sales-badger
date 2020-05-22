0543eaf48f21788d4c98e9b60c3190aaaf699018
alexClifton <oooalexoooo@gmail.com>
2020-05-22 09:49:31 -0700

    in progress

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,8 +13,8 @@ function Footer() {
           <h3>SalesBadger</h3>
         </div>
           <div className="footer-top-right">
-            <div className="columns">
-              <div className="column">
+            <ul className="columns">
+              <li>
                 <h4 className="column-heading">Why SpaceBadger</h4>
                   <ul className="column-list">
                     <li><span>Collaborative Intel</span></li>
@@ -24,8 +24,8 @@ function Footer() {
                     <li><span>cxzczxczxczxc</span></li>
                     <li><span>asdfasdfewthtg</span></li>
                   </ul>
-              </div>
-              <div className="column">
+              </li>
+              <li>
                 <h4 className="column-heading">Product</h4>
                   <ul className="column-list">
                     <li><span>Collaborative Intel</span></li>
@@ -35,8 +35,8 @@ function Footer() {
                     <li><span>cxzczxczxczxc</span></li>
                     <li><span>asdfasdfewthtg</span></li>
                   </ul>
-              </div>
-              <div className="column">
+              </li>
+              <li>
                 <h4 className="column-heading">Company</h4>
                   <ul className="column-list">
                     <li><span>Collaborative Intel</span></li>
@@ -46,39 +46,43 @@ function Footer() {
                     <li><span>cxzczxczxczxc</span></li>
                     <li><span>asdfasdfewthtg</span></li>
                   </ul>
-              </div>
-              <div className="column">
+              </li>
+              <li>
                 <h4 className="column-heading">Contact</h4>
                 <ul className="column-list">
                   <li><span>Collaborative Intel</span></li>
                   <li><span>AI assisntant</span></li>
                   <li><span>loremiupsoiu</span></li>
                 </ul>
+              </li>
+              <li>
                 <h4 className="columns-heading">US Office</h4>
                   <ul className="column-list">
                     <li><span>AI assisntant</span></li>
                     <li><span>loremiupsoiu</span></li>
                   </ul>
+              </li>
+              <li>
                 <h4 className="columns-heading">Singapore Office</h4>
                   <ul className="column-list">
                     <li><span>Collaborative Intel</span></li>
                     <li><span>asdfasdfewthtg</span></li>
                   </ul>
-              </div>
-            </div>
+              </li>
+            </ul>
           </div>
 
       </div>
       <div className="footer-bottom">
         <div className="footer-bottom-top">
-          <div className="footer-bottom-privacy-terms">
-            <span>Privacy</span>
-            <span>Terms</span>
-          </div>
           <div className="social-links">
             <img alt="" src="https://icons.iconarchive.com/icons/sicons/basic-round-social/256/myspace-icon.png"/>
             <img alt="" src="https://lh3.googleusercontent.com/proxy/AXcBU7Wp1Ba0e-kY50FoOzD9gX5pBie3oSHyb3qR8WMlbLVsEodyEXAQK1szT62QIe70IiXI-9pTGnAkpwtqXQ4e_OWIdmGMBQ"/>
             <img alt="" src="https://cdn.logojoy.com/wp-content/uploads/2018/07/05125808/mtv-logo-1024x814.png"/>
+          </div>
+          <div className="footer-bottom-privacy-terms">
+            <span>Privacy</span>
+            <span>Terms</span>
           </div>
         </div>
         <div className="copywrite">
